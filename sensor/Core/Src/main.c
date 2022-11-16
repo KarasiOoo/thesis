14a2865bbf6801b49ab86a69374e7388fdc4bd7a
KarasiOoo <mck.karas@gmail.com>
2022-11-16 02:42:30 +0100

    opt(sensor.c): shorter version of function which overwrite 'HAL_UART_Transmit' with 'printf'

--- sensor/Core/Src/main.c
+++ sensor/Core/Src/main.c
@@ -20,8 +20,7 @@ int __io_putchar(int ch)
 {
 	if(ch == '\n')
 	{
-		uint8_t ch2 = '\r';
-		HAL_UART_Transmit(&huart2, &ch2, 1, HAL_MAX_DELAY);
+		__io_putchar('\r');
 	}
 
 	HAL_UART_Transmit(&huart2, (uint8_t*)&ch, 1, HAL_MAX_DELAY);
